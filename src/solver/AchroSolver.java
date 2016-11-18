4372f360c6d355c863329f4ed74c46b5fae31f38
Jules <Jules>
2016-11-18 15:32:57 +0100

    Solver a faire

--- src/solver/AchroSolver.java
+++ src/solver/AchroSolver.java
@@ -4,16 +4,21 @@ import graphmodel.ColoredNode;
 import org.chocosolver.sat.SatFactory;
 import org.chocosolver.solver.Model;
 import org.chocosolver.solver.Solver;
+import org.chocosolver.solver.constraints.Constraint;
 import org.chocosolver.solver.constraints.nary.cnf.LogOp;
+import org.chocosolver.solver.variables.impl.FixedBoolVarImpl;
+import org.chocosolver.solver.constraints.IIntConstraintFactory;
 import org.chocosolver.solver.search.strategy.Search;
 import org.chocosolver.solver.variables.BoolVar;
 import org.chocosolver.solver.variables.IntVar;
 import org.chocosolver.solver.variables.impl.BoolVarImpl;
 import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.SingleGraph;
 import utils.ColorMapping;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
 /**
  * Created by jules on 16/11/2016.
@@ -45,20 +50,22 @@ public class AchroSolver {
             //le numéro des sommets
             IntVar[] I = model.intVarArray("id domain",N,1, N, false);
             //le domaine des couleurs
-            IntVar[] C = model.intVarArray("color vertex domain",N,0, k, false);
+            IntVar[] C = model.intVarArray("color vertex domain",N,0, k-1, false);
             //le domaines des arêtes, inutile?
-            IntVar[] A = model.intVarArray("edges domain",nbEdges,0, 1, false);
+            IntVar[] A = model.intVarArray("edges domain",N*N,0, 1, false);
 
+            //System.out.println("A.size " + A.length);
             //les contraintes
             model.allDifferent(I).post();
 
             Integer[][] matAdj = new Integer[nbVertexes][nbVertexes];
 
+
             //Construction d'une matrice d'adjacence
             int i1 = 0, i2 = 0, idx=0;
-            for (Object v1 :g.getNodeSet()){
+            for (Node v1 :g.getNodeSet()){
                 ColoredNode s1 = (ColoredNode) v1;
-                for (Object v2 :g.getNodeSet()) {
+                for (Node v2 :g.getNodeSet()) {
                     ColoredNode s2 = (ColoredNode) v2;
                     if (s1.hasEdgeBetween(s2)){
                         matAdj[i1][i2] = 1;
@@ -66,7 +73,7 @@ public class AchroSolver {
                     }
                     else{
                         matAdj[i1][i2] = 0;
-                       // model.arithm(A[idx], "==", 0).post();
+                        //model.arithm(A[idx], "==", 0).post();
                     }
 
                     i2++;
@@ -75,10 +82,10 @@ public class AchroSolver {
                 i2=0;
                 i1++;
             }
-
+            //Constraint c = IIntConstraintFactory.arithm(, "==", );
+            //model.
             //les sommets adjacents n'ont pas la même couleur => couleur prore
             for (int i = 0; i < N ; i++) {
-                model.arithm(C[i], "!=", 1).post();
                 for (int j = 0; j < N; j++) {
                     if ((i != j) && (matAdj[i][j] == 1))
                         model.arithm(C[i], "!=", C[j]).post();
@@ -86,51 +93,93 @@ public class AchroSolver {
             }
 
             //test d'élimination du couleur
-            for (int i = 0; i < N ; i++) {
-                model.arithm(C[i], "!=", 1).post();
-            }
+            //for (int i = 0; i < N ; i++) {
+            //    model.arithm(C[i], "!=", 1).post();
+            //}
 
             //BoolVar a = model.boolVar("a");
             //Pour chaque paire de couleur
 
-            /*LogOp cnf = null;
+
+            /*
+            COntrainte4 = {}
+            Pour chaque couleur Ci1
+                Contrainte3 = {}
+                Pour chaque couleur Ci2
+                    Contrainte2 = {};
+                    Pour chaque noeud Ni1
+                        Contrainte1 + {}
+                        Pour chaque Noeud voisin Ni2 de Ni1
+                            Contrainte1 = Contrainte1 Union (Ni1==Ci1) ET (Ni2==Ci2)
+                        Fin Pour
+                        Contrainte2 = Contrainte2 iNTER Contrainte1
+                    Fin Pour
+                    Contrainte3 = Contrainte3 INTER Contrainte2
+                Fin Pour
+                Contrainte4 = Contrainte4 INTER Contrainte3
+             Fin Pour
+             */
+
+            LogOp contrainte4 = null;
             for (int c1 = 0; c1 < k; c1++) {
+                LogOp contrainte3 = null;
                 for (int c2 = 0; c2 < k; c2++) {
-                    //la clause pour la paire de couleur
-                    LogOp cl = null;
-                    //Pour chaque paire d'aretes
-                    for (int n1 = 0; n1 < N; n1++) {
-                        //n1 a la couleur c1
-                        LogOp dis = null;
-                        BoolVar c1n1 = model.boolVar(""+c1+n1);
-                        for (int n2 = 0; n2 < N; n2++) {
-                            //n2 a la couleur c2
+                    LogOp contrainte2 = null;
+                    for (Node n1 : g.getNodeSet()) {
+                        LogOp contrainte1= null;
+                        Iterator<Node> itNode = n1.getNeighborNodeIterator();
+                        while (itNode.hasNext()){
+                            Node n2 = itNode.next();
+                            BoolVar c1n1 = model.boolVar(""+c1+n1);
+                            // creation de la contrainte
+                            //model.arithm(C[i], "!=", 1).post();
+
                             BoolVar c2n2 = model.boolVar(""+c2+n2);
-                            if (n1 != n2 && matAdj[n1][n2] ==1) {
+                            if (!n1.equals(n2)) {
                                 LogOp conj = LogOp.and(c1n1,c2n2);
-                                if (conj != null){
-                                    if (dis == null){
-                                        dis = conj;
-                                    }
-                                    else {
-                                        dis= LogOp.or(dis,conj);
-                                    }
+                                if (contrainte1 == null){
+                                    contrainte1 = conj;
                                 }
+                                else {
+                                    contrainte1= LogOp.or(contrainte1,conj);
+                                }
+
                             }
+
                         }
-                        if (dis != null){
-                            if (cl == null){
-                                cl = dis;
+                        if (contrainte1 != null){
+                            if (contrainte2 == null){
+                                contrainte2 = contrainte1;
                             }
                             else {
-                                cl= LogOp.and(cl,dis);
+                                contrainte2= LogOp.and(contrainte2,contrainte1);
                             }
                         }
+
                     }
-                    if (cl != null)
-                     model.addClauses(cl);
+                    if (contrainte2 != null){
+                        if (contrainte3 == null){
+                            contrainte3 = contrainte2;
+                        }
+                        else {
+                            contrainte3= LogOp.and(contrainte3,contrainte2);
+                        }
+                    }
                 }
-            }*/
+
+                if (contrainte3 != null){
+                    if (contrainte4 == null){
+                        contrainte4= contrainte3;
+                    }
+                    else {
+                        contrainte4= LogOp.and(contrainte4,contrainte3);
+                    }
+                }
+            }
+
+            if (contrainte4 != null)
+                model.addClauses(contrainte4);
+
 
             Solver solver = model.getSolver();
             //TODO regarder les stratégies
