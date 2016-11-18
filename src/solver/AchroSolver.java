fec2ac9b46f4a06dcc15a7acfb24b0a458814586
Jules <Jules>
2016-11-18 13:06:01 +0100

    TODO faire la formule sat avec des boolarray -> changer le domaine

--- src/solver/AchroSolver.java
+++ src/solver/AchroSolver.java
@@ -4,9 +4,11 @@ import graphmodel.ColoredNode;
 import org.chocosolver.sat.SatFactory;
 import org.chocosolver.solver.Model;
 import org.chocosolver.solver.Solver;
+import org.chocosolver.solver.constraints.nary.cnf.LogOp;
 import org.chocosolver.solver.search.strategy.Search;
 import org.chocosolver.solver.variables.BoolVar;
 import org.chocosolver.solver.variables.IntVar;
+import org.chocosolver.solver.variables.impl.BoolVarImpl;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.SingleGraph;
 import utils.ColorMapping;
@@ -65,7 +67,7 @@ public class AchroSolver {
             //le numéro des sommets
             IntVar[] I = model.intVarArray("id domain",N,1, N, false);
             //le domaine des couleurs
-            IntVar[] C = model.intVarArray("color vertex domain",N,1, N, false);
+            IntVar[] C = model.intVarArray("color vertex domain",N,1, k, false);
             //le domaines des arêtes
             //BoolVar[] V = model.boolVarArray("edges domain",nbEdges);
 
@@ -79,9 +81,46 @@ public class AchroSolver {
                         model.arithm(C[i], "!=", C[j]).post();
                 }
             }
-            for (int i = 0; i < N ; i++) {
-                for (int j = 0; j < N; j++) {
-                    //model.addClauses(and(nand(nor(a, b), or(c, d)), e), model);
+
+            //BoolVar a = model.boolVar("a");
+            //Pour chaque paire de couleur
+
+            LogOp cnf = null;
+            for (int c1 = 0; c1 < k; c1++) {
+                for (int c2 = 0; c2 < k; c2++) {
+                    //la clause pour la paire de couleur
+                    LogOp cl = null;
+                    //Pour chaque paire d'aretes
+                    for (int n1 = 0; n1 < N; n1++) {
+                        //n1 a la couleur c1
+                        LogOp dis = null;
+                        BoolVar c1n1 = model.boolVar(""+c1+n1);
+                        for (int n2 = 0; n2 < N; n2++) {
+                            //n2 a la couleur c2
+                            BoolVar c2n2 = model.boolVar(""+c2+n2);
+                            if (n1 != n2 && matAdj[n1][n2] ==1) {
+                                LogOp conj = LogOp.and(c1n1,c2n2);
+                                if (conj != null){
+                                    if (dis == null){
+                                        dis = conj;
+                                    }
+                                    else {
+                                        dis= LogOp.or(dis,conj);
+                                    }
+                                }
+                            }
+                        }
+                        if (dis != null){
+                            if (cl == null){
+                                cl = dis;
+                            }
+                            else {
+                                cl= LogOp.and(cl,dis);
+                            }
+                        }
+                    }
+                    if (cl != null)
+                     model.addClauses(cl);
                 }
             }
 
@@ -100,7 +139,6 @@ public class AchroSolver {
             }
             //TODO donner toutes les solutions??! ou c'est juste une permutation?
             //TODO REMOVE
-            return;
         }
 
 
