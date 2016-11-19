caf8124397f1d5655e562893118b327049471e6f
Jules <Jules>
2016-11-20 00:50:08 +0100

    fix completude

--- src/test/Test.java
+++ src/test/Test.java
@@ -19,11 +19,13 @@ import java.io.IOException;
  * Created by jules on 16/11/2016.
  */
 public class Test {
+
+    static String GRAPHNAME = "k4";
     public static void main (String[] args) throws IOException {
 
         //AchroSolver.testSolver1();
-        ColoredGraph g = new ColoredGraph("clebsh");
-        GraphReader.buildGraphFromFile(g, "clebsh");
+        ColoredGraph g = new ColoredGraph(GRAPHNAME);
+        GraphReader.buildGraphFromFile(g, GRAPHNAME);
         g.setUiProps();
         //g.addAttribute("ui.stylesheet", Test.class.getClassLoader().getResource("colors.css"));
         //g.display();
