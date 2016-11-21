2ebefede4f8f076c6ef123595cc2f74c3251ee03
Marine RUIZ <Marine RUIZ>
2016-11-21 21:02:08 +0100

    Refactor + Ajout TestBase

--- src/main/java/test/TestBase.java
+++ src/main/java/test/TestBase.java
@@ -5,10 +5,30 @@
  */
 package test;
 
+import graphmodel.ColoredGraph;
+import java.io.IOException;
+import solver.AchroSolverHeuris;
+import utils.GraphReader;
+
 /**
  *
  * @author Marine
  */
 public class TestBase {
-    
+
+    static String GRAPHNAME = "benchmark/marine";
+    public static void main (String[] args) throws IOException {
+
+        //AchroSolver.testSolver1();
+        ColoredGraph g = new ColoredGraph(GRAPHNAME);
+        GraphReader.buildGraphFromFile(g,  GRAPHNAME);
+        g.setUiProps();
+        //g.addAttribute("ui.stylesheet", Test.class.getClassLoader().getResource("colors.css"));
+        //g.display();
+
+        AchroSolverHeuris solver = new AchroSolverHeuris();
+        int achroNumber = solver.solve(g);
+        System.out.println("Achromatic number : " + achroNumber);
+        g.display();
+    }
 }
