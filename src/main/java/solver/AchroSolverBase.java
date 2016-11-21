9e9388e99e6aa22582443cf83f2671c496fff73f
Marine RUIZ <Marine RUIZ>
2016-11-21 20:45:43 +0100

    Modif de AchroSolverBase avec remise de l'ancien code

--- src/main/java/solver/AchroSolverBase.java
+++ src/main/java/solver/AchroSolverBase.java
@@ -7,6 +7,12 @@ import org.graphstream.graph.implementations.SingleGraph;
 import utils.ColorMapping;
 
 import java.util.*;
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.Solver;
+import org.chocosolver.solver.constraints.Constraint;
+import org.chocosolver.solver.constraints.nary.cnf.LogOp;
+import org.chocosolver.solver.variables.BoolVar;
+import org.chocosolver.solver.variables.IntVar;
 
 /**
  * Cette classe code la résolution de la coloration, 
@@ -21,126 +27,204 @@ public class AchroSolverBase {
 
     private final static int TIME_LIMIT = 60;
 
-    public int solve(SingleGraph g){
+    public int solve(SingleGraph g) {
+        // booleen pour savoir si nous avons fini l'attribution de couleurs
         boolean hasBeenComplete = false;
+        
+        // recuperation de donnees sur le graphe
         Integer nbEdges = g.getEdgeCount();
         Integer nbVertexes = g.getNodeCount();
 
         //Determination des bornes pour k, le nombre achromatique
-        //La borne sup est le nombre de noeud (grossier)
         Integer bSupNbAchro = g.getNodeSet().size();
-        //La borne inf est la taille de la clique maximale (grossier)
         Integer bInfNbAchro = 0;
+        
         //Pour respecter la contrainte de coloration propre
-        List<Node> maximalClique = new ArrayList<Node>();
-        List<List<Node>> maximalCliques = new ArrayList<>();
-        List<ColoredNode> unionClique = new ArrayList<>();
-        int maxAppClique = 0;
-        boolean getMax = true;
-        ColoredNode maxNode = null;
-        for (List<Node> clique : Toolkit.getMaximalCliques(g)) {
-            if (getMax){
-                maxNode = (ColoredNode) clique.get(0);
-                getMax = false;
-            }
-            if (clique.contains(maxNode))
-                maxAppClique++;
-            maximalCliques.add(clique);
-            System.out.println("maxclique"+ clique.size());
-            if (clique.size() > maximalClique.size())
-                maximalClique = clique;
-
-            for (Node node : clique){
-                ColoredNode cNode = (ColoredNode) node;
-                if (!cNode.equals(maxNode))
-                    unionClique.add(cNode);
-            }
+        Integer maxDegree = 0;
+        for (Object v :g.getNodeSet()) {
+            ColoredNode s = (ColoredNode) v;
+            if (s.getDegree() > maxDegree)
+                maxDegree = s.getDegree();
         }
-
-        bInfNbAchro = maximalClique.size();
-
-        ColoredNode[] sortedNodes = new ColoredNode[nbVertexes];
-        int cpt=0;
-        for (Node node : g.getNodeSet()){
-            sortedNodes[cpt]=(ColoredNode) node;
-            cpt++;
-        }
-        Arrays.sort(sortedNodes, (o1, o2) -> {
-            if (o1.getDegree()>o2.getDegree()){
-                return -1;
-            }
-            else if (o1.getDegree()<o2.getDegree()){
-                return 1;
-            }
-            return 0;
-        });
-        //System.out.println(Arrays.toString(sortedNodes));
+        bInfNbAchro = maxDegree;
 
         int N = nbVertexes;
 
-        /*Integer[] mapping = new Integer[nbVertexes];
-        Integer[] mapping2 = new Integer[nbVertexes];
-        int cpt2 = 0;
-        for (Node sortedNode: sortedNodes){
-            mapping[sortedNode.getIndex()] = cpt2;
-            mapping2[cpt2] = sortedNode.getIndex();
-            cpt2++;
-        }*/
-
-
         for (int k = bInfNbAchro; k <= bSupNbAchro; k++){
-            AchroSolverk solveur = new AchroSolverk(g, k, N, maximalCliques, sortedNodes);
+            Model model = new Model("All complete coloring of size " + k);
+            
+            // On definit le domaine des couleurs
+            IntVar[] CouLeur = model.intVarArray("color vertex domain", N, 0, k-1, false);
+            
+            // On definit le domaines des arêtes
+            IntVar[] AreTe = model.intVarArray("edges domain", nbEdges, 0, k*(k-1), false);
 
-            if(solveur.solve()){
-                hasBeenComplete = true;
-                System.out.println("Une solution a été trouvé pour le nombre achromatique " + k);
-                for (int i = 0; i < N ; i++) {
-                    int color = solveur.B[i].getValue();
-                    System.out.println("L'arete "+i+" est de couleur "+ColorMapping.colorsMap[color%32]);
-                    //g.getNode(i).addAttribute("ui.class", "color" + i);
-                    g.getNode(/*mapping2[*/i/*]*/).addAttribute("ui.style", "fill-color: " + ColorMapping.colorsMap[color%32]+";");
+            // On definit le tableau rempli en respectant les contraintes
+            BoolVar[] B = model.boolVarArray("is the vertex has the color c", nbEdges*k);
+
+            // On definit une matrice d'adjacence que l'on construit
+            Integer[][] matAdj = new Integer[N][N];
+
+            int i1 = 0, i2 = 0;
+            for (Node v1 :g.getNodeSet()){
+                ColoredNode s1 = (ColoredNode) v1;
+                for (Node v2 :g.getNodeSet()) {
+                    ColoredNode s2 = (ColoredNode) v2;
+                    if (s1.hasEdgeBetween(s2))
+                        matAdj[i1][i2] = 1;
+                    else
+                        matAdj[i1][i2] = 0;
+                    i2++;
+                }
+                i2=0;
+                i1++;
+            }
+            
+            // A partir de la matrice d'adjacence on peut coder les contraintes de la coloration propre
+            // Les sommets adjacents n'ont pas la même couleur => coloration propre
+            for (int i = 0; i < N ; i++) {
+                for (int j = 0; j < N; j++) {
+                    if ((i != j) && (matAdj[i][j] == 1))
+                        model.arithm(CouLeur[i], "!=", CouLeur[j]).post();
+                }
+            }
+
+            // Chaque sommet ne peut avoir qu'une couleur !!
+            for (int i = 0; i < N ; i++) {
+                for (int c=0;c<k;c++){
+                    for(int g2 = 0; g2 < k; g2++){
+                        if(g2 != c) {
+                            Constraint constr = model.arithm(B[(i * k) + c], "!=", B[(i * k) + g2]);
+                            model.ifThen(B[(i * k) + c], constr);
+                        }
+                    }
+                }
+            }
+
+            //coloration propre
+            for (int i = 0; i < N-1 ; i++) { // pour chaque noeud
+                for (int j = i+1; j < N ; j++) { // pour chaque couleur
+                    for (int c=0; c<k;c++) {
+                        if (matAdj[i][j]==1) {
+                            Constraint constr = model.arithm(B[(i*k)+c], "!=", B[(j*k)+c]);
+                            model.ifThen(B[(i*k)+c], constr);
+                        }
+                    }
+                }
+            }
+
+            for (int i=0; i<N;i++){
+                BoolVar[] BTemp = new BoolVar[k];
+                for (int coul=0; coul<k;coul++) {
+                    BTemp[coul]=B[i*k+coul];
+                }
+                model.notAllEqual(BTemp).post();
+            }
+
+            /*for(int i = 0; i < N-1; i++) {
+                for(int j = i+1; j < N-1; j++) {
+                    for(int l = 0; l < k; l++) {
+                        if(matAdj[i][j] == 1) {
+                            model.ifThen(B[i+l],model.arithm(B[i+l], "!=", B[j+l]));
+                        }
+                    }
+                }
+            }*/
+
+
+            int idxN1 = 0;
+            int idxN2 = 0;
+            LogOp contrainte4 = null;
+            for (int c1 = 0; c1 < k; c1++) {
+                LogOp contrainte3 = null;
+                for (int c2 = 0; c2 < k; c2++) {
+                    LogOp contrainte2 = null;
+                    idxN1=0;
+                    for (Node n1 : g.getNodeSet()) {
+                        LogOp contrainte1= null;
+                        idxN2=0;
+                        for (Node n2 : g.getNodeSet()) {
+                            if (matAdj[idxN1][idxN2]==1) {
+                                /*Constraint constraint1 = model.arithm(C[idxN1], "=", c1);
+                                Constraint constraint2 = model.arithm(C[idxN2], "=", c2);
+                                BoolVar c1n1 = constraint1.reify();
+                                // creation de la contrainte
+                                BoolVar c2n2 = constraint2.reify();*/
+
+                                BoolVar c1n1 =B[idxN1*c1];
+                                BoolVar c2n2 =B[idxN2*c2];
+
+                                if (!n1.equals(n2)) {
+                                    LogOp conj = LogOp.and(B[idxN1*c1], B[idxN2*c2]);
+                                    if (contrainte1 == null) {
+                                        contrainte1 = conj;
+                                    } else {
+                                        contrainte1 = LogOp.or(contrainte1, conj);
+                                    }
+                                }
+                            }
+                            idxN2++;
+                        }
+                        if (contrainte1 != null){
+                            if (contrainte2 == null){
+                                contrainte2 = contrainte1;
+                            }
+                            else {
+                                contrainte2= LogOp.or(contrainte2,contrainte1);
+                            }
+                        }
+                        idxN1++;
+                    }
+                    if (contrainte2 != null){
+                        if (contrainte3 == null){
+                            contrainte3 = contrainte2;
+                        }
+                        else {
+                            contrainte3= LogOp.and(contrainte3,contrainte2);
+                        }
+                    }
                 }
 
-                //Affichage personnalise selon le graphe
-                //clebshLayout(g);
+                if (contrainte3 != null){
+                    if (contrainte4 == null){
+                        contrainte4= contrainte3;
+                    }
+                    else {
+                        contrainte4= LogOp.and(contrainte4,contrainte3);
+                    }
+                }
+            }
 
-                //Affichage de toutes les solutions : nécessaire? Attention JAVA HEAP SPACE
-                /*solver.findAllSolutions();
-                solver.showSolutions();
-                solver.showStatistics();*/
+            if (contrainte4 != null)
+                model.addClauses(contrainte4);
 
-                //Affichage en continu
-                // g.display();
+
+            Solver solver = model.getSolver();
+            //TODO regarder les stratégies
+            //solver.setSearch(Search.minDomLBSearch(C));
+
+            if(solver.solve()){
+                System.out.println("All complete coloring of number achromatic :" + k);
+                for (int i = 0; i < N ; i++) {
+                    int color = 0;
+                    for (int c=0;c<k;c++){
+                        System.out.println(B[(i*k)+c].getValue());
+                        if (B[(i*k)+c].getValue() == 1){
+                            color = c;
+                        }
+                    }
+                    System.out.println("L'arete "+i+" est de couleur "+ColorMapping.colorsMap[color]);
+                    //g.getNode(i).addAttribute("ui.class", "color" + i);
+                    g.getNode(i).addAttribute("ui.style", "fill-color: " + ColorMapping.colorsMap[color]+";");
+                }
+                g.display();
             }
             else {
-                int runtime = solveur.runtime;
-                if (runtime >= TIME_LIMIT){
-                    if (k>bInfNbAchro && hasBeenComplete) {
-                        int nbachro = k-1;
-                        System.out.print("Le solveur n'a pas pu trouver de solutions dans le temps limite de " + (TIME_LIMIT) + " secondes, ");
-                        System.out.println("le nombre achromatique est " + "au moins egal a " + nbachro);
-                        return k - 1;
-                    }
-                    else{
-                        System.out.println("Le solveur n'a pas pu determiner s'il existait une coloration complete" + " en " + (TIME_LIMIT) +" secondes");
-                        return -1;
-                    }
-                }
-                else {
-                    if (k > bInfNbAchro && hasBeenComplete) {
-                        int nbachro = k - 1;
-                        System.out.println("Le nombre achromatique du graphe est " + "egal a " + nbachro);
-                        return nbachro;
-                    } else if (k == bSupNbAchro) {
-                        System.out.println("Le graphe n'admet pas de coloration complete");
-                        return -1;
-                    }
-                }
-                //TODO traiter le else
+                System.out.println("il n'y a pas de solutions");
             }
-
-            //TODO donner toutes les solutions? ou c'est juste une permutation?
+            //TODO donner toutes les solutions??! ou c'est juste une permutation?
         }
+
         return bSupNbAchro;
     }
 }
