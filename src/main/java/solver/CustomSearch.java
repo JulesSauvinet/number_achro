67a486eabae6b0fdbf1e2dfa5089bba9fbb8057f
JulesSauvinet <julessauvinet@yahoo.fr>
2016-11-22 15:40:09 +0100

    Nettoyage

---
+++ src/main/java/solver/CustomSearch.java
@@ -0,0 +1,15 @@
+package solver;
+
+import org.chocosolver.solver.search.strategy.Search;
+import org.chocosolver.solver.search.strategy.decision.Decision;
+import org.chocosolver.solver.search.strategy.strategy.AbstractStrategy;
+
+/**
+ * Created by jules on 22/11/2016.
+ */
+public class CustomSearch extends AbstractStrategy {
+    @Override
+    public Decision getDecision() {
+        return null;
+    }
+}
